338f05c507e7a7429482449be788f77371798ca5
rezwanul24 <rezwanul.shajib@hotmail.com>
2023-01-30 15:09:08 +0600

    registation setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
 import { route } from './routes/Route';
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer></ToastContainer>
       <RouterProvider router={route}></RouterProvider>
     </div>
   );
